a6f4ed893b478bc7f93a822f33cb09d1bea9b54b
Danny <Barrantes>
2020-11-23 04:19:35 -0600

    Layout using flex

--- App.js
+++ App.js
@@ -19,14 +19,30 @@ import {
 
 const App: () => React$Node = () => {
   return (
-  <View>
-  <Text>Hola Mundo!</Text>
+  <View style={styles.container}>
+  <Text style={styles.welcome}>Hola Mundo!</Text>
+  <Text style={[styles.welcome,{flex: 1,}]}>Hola Mundo!</Text>
+  <Text style={styles.welcome}>Hola Mundo!</Text>
   </View>
   );
 };
 
 const styles = StyleSheet.create({
- 
+ container: {
+flex: 1,
+justifyContent: 'space-around',
+//alignItems: 'center',
+backgroundColor: 'orange',
+
+ },
+ welcome: {
+   flex:2,
+   backgroundColor: 'pink',
+   fontSize: 20,
+ //  textAlign: 'center',
+   margin: 10,
+
+ }
 });
 
 export default App;
